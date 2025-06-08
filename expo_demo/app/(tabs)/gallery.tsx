e3f58c07fb3e95cd577055067cacd2b92a6984d6
danny <dannytakushi@gmail.com>
2025-06-07 21:21:44 -0500

    basic gallery image integration

--- expo_demo/app/(tabs)/gallery.tsx
+++ expo_demo/app/(tabs)/gallery.tsx
@@ -9,6 +9,15 @@ import * as React from 'react';
 import { supabase } from '../../lib/supabase'
 import { Session } from '@supabase/supabase-js'
 import { DataTable } from 'react-native-paper';
+import { Timestamp } from 'react-native-reanimated/lib/typescript/commonTypes';
+
+type gallery_images = {
+  id: number;
+  created_at: Timestamp;
+  name: string;
+  description: string;
+  url: string;
+};
 
 type Profile = {
   username: string;
@@ -20,7 +29,10 @@ export default function TabFourScreen() {
   const [session, setSession] = React.useState<Session | null>(null)
   const [profiles, setProfiles] = React.useState<Profile[] | null>([]);
   const [imageUrls, setImageUrls] = React.useState<string[]>([]);
+  const [galleryImages, setGalleryImages] = React.useState<gallery_images[] | null>([]);
+  const [galleryImageUrls, setGalleryImageUrls] = React.useState<string[]>([]);
   const avatarSize = { height: 150, width: 150 }
+  const galleryImageSize = { height: 800, width: 1200 }
 
   React.useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -30,6 +42,7 @@ export default function TabFourScreen() {
       setSession(session)
     })
     getProfiles();
+    getGalleryImages();
   }, []);
 
   // gets profiles
@@ -78,6 +91,51 @@ export default function TabFourScreen() {
     }
   }
 
+  // gets profiles
+  async function getGalleryImages() {
+    const { data } = await supabase.from("gallery_images").select();
+    console.log("galleryImages", data);
+    
+    // add updateFullUrl process for each profile object
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].url) {
+        await updateGalleryFullUrl(data[i].url, i);
+        
+        data[i].url = "https://utlankoqlpvjmwacdzai.supabase.co/storage/v1/object/public/gallerypictures/" + data[i].url;
+      } else {
+      }
+    }
+
+    setGalleryImages(data);
+  }
+  // updates galleryImageUrls asynchronously`
+  async function updateGalleryFullUrl(path: string, idx: number) {
+    console.log("updateGalleryFullUrl for idx", idx, "path", path);
+    var fullUrlPath = "";
+    try {
+      const { data, error } = await supabase.storage.from('gallerypictures').download(path)
+
+      if (error) {
+        throw error
+      }
+
+      const fr = new FileReader()
+      fr.readAsDataURL(data)
+      fr.onload = () => {
+        fullUrlPath = fr.result as string
+
+        const newGalleryImageUrls = [...galleryImageUrls];
+        newGalleryImageUrls[idx] = fullUrlPath;
+        setGalleryImageUrls(newGalleryImageUrls);
+        console.log("galleryImageUrls", galleryImageUrls);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('Error downloading image: ', error.message)
+      }
+    }
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
@@ -93,7 +151,24 @@ export default function TabFourScreen() {
         <ThemedText type="title">gallery</ThemedText>
       </ThemedView>
 
+      {galleryImages?.map((galleryImage) => (
+        <View key={galleryImage.id} style={{ margin: 10 }}>
+          <Image
+            // source={{ uri: galleryImageUrls[galleryImages.indexOf(galleryImage)] }}
+            source={{ uri: galleryImage.url }}
+            accessibilityLabel="Gallery Image"
+            style={[galleryImageSize, styles.avatar, styles.image]}
+          // onLoad={() => updateGalleryFullUrl(galleryImage.image_url, galleryImages.indexOf(galleryImage))}// TODO :attempt using onLoad to complete the image URL
+          />
+          <ThemedText>{galleryImage.name}</ThemedText>
+          <ThemedText>{new Date(galleryImage.created_at).toLocaleDateString()}</ThemedText>
+          <ThemedText>{galleryImage.description}</ThemedText>
+        </View>
+      ))}
 
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">team</ThemedText>
+      </ThemedView>
       <DataTable>
         <DataTable.Header>
           <DataTable.Title><ThemedText>name</ThemedText></DataTable.Title>
